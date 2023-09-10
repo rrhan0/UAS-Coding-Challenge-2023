2f91f2e3e0830d002004585d81c890af781dbcb1
rrhan0 <rrhan2002@gmail.com>
2023-09-09 21:59:43 -0700

    Connect function completed + react initializations

--- coding_challenge/server/models.py
+++ coding_challenge/server/models.py
@@ -1,3 +1,5 @@
 from django.db import models
+from dronekit import connect
 
 # Create your models here.
+
--- coding_challenge/server/views.py
+++ coding_challenge/server/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+import json
 
+from django.shortcuts import render
+from django.http import JsonResponse
+from dronekit import connect
+from . import constants
 # Create your views here.
+
+
+def index(request):
+    data = {
+        'message': 'Hello World'
+    }
+    return JsonResponse(data)
+
+
+def connect_drone(request):
+    port = 5760
+    vehicle = connect(ip=f'{constants.DRONE_IP}:{port}', wait_ready=True)
+    data = {'state': vehicle.system_status.__str__()}
+    return JsonResponse(data)
+
+
+
